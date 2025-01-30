2493bf2012ab01e82f8153de393f07e0fdeef50f
somaharini <somaharini24@gmail.com>
2025-01-30 22:09:35 +0530

    Create Multiplicationtable.java

---
+++ Multiplicationtable.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Multiplicationtable {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number to display its multiplication table: ");
+        int number = scanner.nextInt();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " x " + i + " = " + (number * i));
+        }
+        scanner.close();
+    }
+}
